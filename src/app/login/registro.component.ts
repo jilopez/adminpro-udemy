c5fadc110fc0b0bd467afb6afc3e31f3f0ca66d9
Jose Lopez <jilopez800502@gmail.com>
2019-12-22 13:43:41 -0800

    Primer commit

---
+++ src/app/login/registro.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { LoginComponent } from './login.component';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class RegistroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
